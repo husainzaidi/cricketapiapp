dc9e33e7965864a8c3cfa02321dae0d5d53e0790
husainzaidi <er.husain2017@gmail.com>
2022-03-04 23:50:50 +0530

    make it betterj

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 const express = require("express");
+const { json } = require("express/lib/response");
 const res = require("express/lib/response");
 const app = express();
-const importData = require("./data.json");
+const news = require("./data.json");
+const testresult = require("./testresult.json");
+const quiz = require("./quizgames.json");
+const stadiums = require("./stadium.json");
+const upcomingodi = require("./upcomingodi.json");
+const upcomingt20 = require("./upcomingt20.json");
+const upcomingtest = require("./upcomingtest.json");
 let port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
-    res.send("hello world");
+    res.send("www.google.com");
 });
 
-app.get("/players", (req, res) => {
-    res.send(importData);
+app.get("/news", (req, res) => {
+    res.send(news);
 })
+app.get("/overalldetails", (req, res) => {
+    res.send(overalldetails);
+});
+app.get("/quiz", (req, res) => {
+    res.send(quiz);
+});
+app.get("/stadium", (req, res) => {
+    res.send(stadiums);
+});
+app.get("/testresult", (req, res) => {
+    res.send(testresult);
+});
+app.get("/upcomingodi", (req, res) => {
+    res.send(upcomingodi);
+});
+app.get("/upcomingt20", (req, res) => {
+    res.send(upcomingt20);
+});
+app.get("/upcomingtest", (req, res) => {
+    res.send(upcomingtest);
+});
 
 
 app.listen(port, () => {
